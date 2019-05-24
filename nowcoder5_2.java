2df7cdaa3c2689c58438bf103d82018d7ddd56d2
questionn <610347004@qq.com>
2019-05-24 22:17:52 +0800

    每日一题5———2

---
+++ nowcoder5_2.java
@@ -0,0 +1,19 @@
+//两个栈实现一个队列
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+    public void push(int node) {
+        stack1.push(node);
+    }
+
+    public int pop() {
+        int tmp = 0;
+        if (!stack2.empty()) {
+            return stack2.pop();
+        } else {
+            while (!stack1.empty()) {
+                stack2.push(stack1.pop());
+            }
+            tmp = stack2.pop();
+        }
+        return tmp;
+        }
